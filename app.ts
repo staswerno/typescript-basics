bfd2399fcdf4323ba6fcac738bd24104f5576226
Staś Werno <89650713+staswerno@users.noreply.github.com>
2022-03-23 18:43:37 +0100

    finish function types

--- app.ts
+++ app.ts
@@ -8,6 +8,13 @@ function printResult2(num: number): void {
 	console.log("Result: " + num);
 }
 
+//void return type here means, if you add a return, nothing will be returned
+//but you won't get punished for it
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+	const result = n1 + n2;
+	cb(result);
+}
+
 printResult2(add2(5, 12));
 
 // let combineValues: Function;
@@ -16,3 +23,7 @@ let combineValues: (a: number, b: number) => number;
 combineValues = add2;
 
 console.log(combineValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+	console.log(result);
+});
