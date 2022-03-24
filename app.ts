cc807b4cebbf4f3e5128b1eca94d529f69a3e569
Staś Werno <89650713+staswerno@users.noreply.github.com>
2022-03-24 12:01:49 +0100

    add unknown and never types

--- app.ts
+++ app.ts
@@ -1,30 +1,24 @@
-//return type
-function add2(n1: number, n2: number): number {
-	return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "Max";
+
+// userName = userInput
+// switch unknown to any to remove error
+// unknown more restrictive
+
+if (typeof userInput === "string") {
+	userName = userInput;
 }
 
-//return type void
-function printResult2(num: number): void {
-	console.log("Result: " + num);
+function generateError(message: string, code: number): never {
+	throw {
+		message: message,
+		errorCode: code,
+	};
 }
+// throw cancels script so actual type is 'never'
 
-//void return type here means, if you add a return, nothing will be returned
-//but you won't get punished for it
-//so! good to use void
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-	const result = n1 + n2;
-	cb(result);
-}
-
-printResult2(add2(5, 12));
-
-// let combineValues: Function;
-let combineValues: (a: number, b: number) => number;
-
-combineValues = add2;
-
-console.log(combineValues(8, 8));
-
-addAndHandle(10, 20, (result) => {
-	console.log(result);
-});
+const result = generateError("Error! Error! Panic!", 500);
+console.log(result);
---
+++ functions.ts
@@ -0,0 +1,30 @@
+//return type
+function add2(n1: number, n2: number): number {
+	return n1 + n2;
+}
+
+//return type void
+function printResult2(num: number): void {
+	console.log("Result: " + num);
+}
+
+//void return type here means, if you add a return, nothing will be returned
+//but you won't get punished for it
+//so! good to use void
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+	const result = n1 + n2;
+	cb(result);
+}
+
+printResult2(add2(5, 12));
+
+// let combineValues: Function;
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add2;
+
+console.log(combineValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+	console.log(result);
+});
